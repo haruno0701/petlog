be61c0e003aae7fc77fa99526efd1cbcf11e07d2
haruno naduki <naduki.h.0701@gmail.com>
2025-03-05 11:31:07 +0000

    ハンバーガーボタン

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            
+                        <li><a class="nav-link" href="{{ route('login') }}">{{ __('messages.login') }}</a></li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
